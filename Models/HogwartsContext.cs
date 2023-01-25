9d5cdbcd7fc4edfa2873dff68e5bbc76dc68bb58
tercsd <tercsd@gmail.com>
2023-01-25 14:41:52 +0100

    implement exploratory brewing

--- Models/HogwartsContext.cs
+++ Models/HogwartsContext.cs
@@ -1,7 +1,6 @@
 using HogwartsPotions.Models.Entities;
 using HogwartsPotions.Models.Enums;
 using Microsoft.EntityFrameworkCore;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -106,22 +105,146 @@ namespace HogwartsPotions.Models
 
         public async Task<Potion> AddBrewingPotion(Recipe potion)
         {
-            throw new NotImplementedException();
+            if (potion.Ingredients.Any())
+            {
+                var newPotion = new Potion()
+                {
+                    Name = potion.Name,
+                    Maker = await Students.Where(s => s.ID == potion.Maker.ID).FirstAsync(),
+                    Ingredients = new HashSet<Ingredient>(),
+                };
+
+                foreach (var ingredient in potion.Ingredients)
+                {
+                    if (!Ingredients.Any(i => i.Name == ingredient.Name))
+                    {
+                        await Ingredients.AddAsync(ingredient);
+                        newPotion.Ingredients.Add(ingredient);
+                    }
+                    else
+                    {
+                        var existingIngredient = await Ingredients.Where(i => i.Name == ingredient.Name).FirstAsync();
+                        newPotion.Ingredients.Add(existingIngredient);
+                    }
+                }
+
+                if (newPotion.Ingredients.Count < MaxIngredientsForPotions)
+                {
+                    newPotion.BrewingStatus = BrewingStatus.Brew;
+                }
+                else
+                {
+                    var foundRecipes = (await Recipes.Include(r => r.Ingredients).ToListAsync())
+                        .Where(r => r.Ingredients.ToHashSet()
+                        .SetEquals(newPotion.Ingredients.ToHashSet()))
+                        .ToList();
+                    if (foundRecipes.Count > 0)
+                    {
+                        newPotion.BrewingStatus = BrewingStatus.Replica;
+                        string replicaPotionName = $"{foundRecipes.First().Name} replica";
+                        newPotion.Name = replicaPotionName;
+                    }
+                    else
+                    {
+                        int discoveryCount = 1;
+                        while ((await Recipes.ToListAsync()).Any(r => r.Name == $"{newPotion.Maker.Name}'s discovery #{discoveryCount}"))
+                        {
+                            discoveryCount++;
+                        }
+                        string newPotionName = $"{newPotion.Maker.Name}'s discovery #{discoveryCount}";
+                        newPotion.BrewingStatus = BrewingStatus.Discovery;
+                        newPotion.Name = newPotionName;
+
+                        var discoveredRecipe = new Recipe()
+                        {
+                            Maker = newPotion.Maker,
+                            Name = newPotionName,
+                            Ingredients = newPotion.Ingredients.ToHashSet(),
+                        };
+                        await Recipes.AddAsync(discoveredRecipe);
+                    }
+                }
+
+                await Potions.AddAsync(newPotion);
+                await SaveChangesAsync();
+                return newPotion;
+            }
+            return null;
         }
 
         public async Task<Potion> AddToPotion(long potionId, Ingredient ingredient)
         {
-            throw new NotImplementedException();
+            var potion = await Potions.Include(r => r.Ingredients).Include(r => r.Maker).Where(p => p.ID == potionId).FirstOrDefaultAsync();
+
+            if (!Ingredients.Any(i => i.Name == ingredient.Name))
+            {
+                await Ingredients.AddAsync(ingredient);
+                potion.Ingredients.Add(ingredient);
+            }
+            else
+            {
+                var existingIngredient = await Ingredients.Where(i => i.Name == ingredient.Name).FirstAsync();
+                potion.Ingredients.Add(existingIngredient);
+            }
+
+            if (potion.Ingredients.Count < 5)
+            {
+                potion.BrewingStatus = BrewingStatus.Brew;
+
+            }
+            else
+            {
+                var foundRecipes = (await Recipes.Include(r => r.Ingredients).ToListAsync())
+                    .Where(r => r.Ingredients.ToHashSet()
+                    .SetEquals(potion.Ingredients.ToHashSet()))
+                    .ToList();
+                if (foundRecipes.Count > 0)
+                {
+                    potion.BrewingStatus = BrewingStatus.Replica;
+                    string replicaPotionName = $"{foundRecipes.First().Name} replica";
+                    potion.Name = replicaPotionName;
+                }
+                else
+                {
+                    int discoveryCount = 1;
+                    while ((await Recipes.ToListAsync()).Any(r => r.Name == $"{potion.Maker.Name}'s discovery #{discoveryCount}"))
+                    {
+                        discoveryCount++;
+                    }
+                    string newPotionName = $"{potion.Maker.Name}'s discovery #{discoveryCount}";
+                    potion.BrewingStatus = BrewingStatus.Discovery;
+                    potion.Name = newPotionName;
+
+                    var discoveredRecipe = new Recipe()
+                    {
+                        Maker = potion.Maker,
+                        Name = newPotionName,
+                        Ingredients = potion.Ingredients.ToHashSet(),
+                    };
+                    await Recipes.AddAsync(discoveredRecipe);
+                }
+            }
+            await SaveChangesAsync();
+            return potion;
         }
 
         public async Task<Potion> GetPotionById(long potionId)
         {
-            throw new NotImplementedException();
+            return await Potions.Include(p => p.Maker).Include(p => p.Ingredients)
+                .AsNoTracking().Where(p => p.ID == potionId).SingleAsync();
         }
 
         public async Task<List<Recipe>> GetPotionHelpById(long potionId)
         {
-            throw new NotImplementedException();
+            var potion = await Potions.Include(r => r.Ingredients)
+                .Where(p => p.ID == potionId)
+                .FirstOrDefaultAsync();
+            var recipes = await Recipes.Include(r => r.Ingredients)
+                .ToListAsync();
+            var matchingRecipes = recipes
+                .Where(r => potion.Ingredients.ToHashSet().IsSubsetOf(r.Ingredients.ToHashSet()))
+                .ToList();
+            return await Recipes.AsNoTracking().Where(r => matchingRecipes.Contains(r)).ToListAsync();
         }
 
         public async Task AddRoom(Room room)
